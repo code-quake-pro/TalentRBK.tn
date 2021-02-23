4c6a9d0f4ae08c0569a499953d5c1df7fc395aa9
Hbib24 <habib.bekir@gmail.com>
2021-02-23 14:32:18 +0100

    company account creation

---
+++ front/src/components/AccountForm.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function AccountForm() {
+  const [email, setEmail] = useState("");
+
+  var submit = () => {
+    axios
+      .post("http://localhost:3001/api/user/new", {
+        email: email
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
+  return (
+    <div>
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type="submit" onClick={() => submit()}>
+        Create
+      </button>
+    </div>
+  );
+}
+
+export default AccountForm;
